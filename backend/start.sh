5a7763cb122ef2349c1ee8a8cf1054a7f305b3b5
Somrik Mondal <mondalsomrik69@gmail.com>
2026-02-27 21:45:50 +0530

    Fixed minor issues

--- backend/start.sh
+++ backend/start.sh
@@ -1,2 +1,7 @@
-#!/bin/bash
-uvicorn api.main:app --host 0.0.0.0 --port 10000 --workers 1
+#!/usr/bin/env bash
+
+export OMP_NUM_THREADS=1
+export OPENBLAS_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+
+uvicorn main:app --host 0.0.0.0 --port 10000
